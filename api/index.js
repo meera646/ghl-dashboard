66de549e598579095e23eb1337e24f167c2839a6
meera646 <meeraarthisri4@gmail.com>
2026-03-23 12:56:53 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const axios = require('axios');
 
-const app = express();
+module.exports = async (req, res) => {
+  const token = process.env.GHL_API_KEY; 
+  const locationId = 'xhikX0m94bzYmA2CBwwd'; // This is your specific account ID
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Vercel Serverless API' });
-});
-
-module.exports = app;
+  try {
+    const response = await axios.get(`https://services.leadconnectorhq.com/emails/stats/location/${locationId}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Version': '2021-07-28'
+      }
+    });
+    res.status(200).json({ emailCount: response.data.totalCount || 0 });
+  } catch (error) {
+    res.status(500).json({ emailCount: 0 }); // If it fails, show 0
+  }
+};
